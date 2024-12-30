97ddce2ebc223ce10235c5a786e5232de1241339
ADMIN <ADMIN@DESKTOP-56LNQVK>
2024-12-30 16:28:51 +0530

    commit2

--- src/ikcon/ikcon.java
+++ src/ikcon/ikcon.java
@@ -26,21 +26,7 @@ public class ikcon {
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			js.executeScript("document.getElementsByTagName('a')[8].click()");
 			
-			/*//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			WebElement e=driver.findElement(By.xpath("//a[text()='Decline']"));
-		    e.click();
-			WebElement e1=driver.findElement(By.xpath("(//div[@class='nzVyrk'])[1]"));
-			Actions ac=new Actions(driver);
-			ac.moveToElement(e1).perform();
-		
-			 WebElement invisibleOption = driver.findElement(By.xpath("//nav[@id='comp-le6xamw0navContainer']/ul/li[1]/ul/li[3]/a"));
-		        JavascriptExecutor js = (JavascriptExecutor) driver;
-		        js.executeScript("arguments[0].click();", invisibleOption);*/
-
-			
-	       
-	     
-	        	
+			 	
 	        }
 
 			
